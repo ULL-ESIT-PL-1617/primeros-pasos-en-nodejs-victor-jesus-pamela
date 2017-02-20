06ce547b146ce7f894ba9a27c92e9e7918e16295
jeplasenciap <jeplasenciap@gmail.com>
2017-02-20 13:02:06 +0000

    Añadida la tarea al gulpfile para desplegar el libro en gh-pages.

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,10 @@
 var gulp = require('gulp');
+var gh-pages = require('gulp-gh-pages');
 
 gulp.task('default', function() {
 
+});
+
+gulp.task('deploy', function() {
+  return gulp.src('./_book/**/*').pipe(gh-pages());
 });
