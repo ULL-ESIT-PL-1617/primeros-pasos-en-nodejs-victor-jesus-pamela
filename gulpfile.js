99b0fde663412623603d7d48de4e44ef121cf8d2
alu0100821215 <alu0100821215@ull.edu.es>
2017-02-20 14:50:40 +0000

    Añadida la instrucción build en el Gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -5,6 +5,19 @@ gulp.task('default', function() {
 
 });
 
+gulp.task('build', function() {
+  var exec = require('child_process').exec;
+    var child;
+    child = exec("npm run generate-gitbook", 
+                function (error, stdout, stderr) {
+      console.log('stdout: ' + stdout);
+      console.log('stderr: ' + stderr);
+      if (error !== null) {
+        console.log('exec error: ' + error);
+      }
+    });
+});
+
 gulp.task('deploy', function() {
   return gulp.src('./gh-pages/**/*').pipe(ghPages());
 });
