14214449b36b5e4a1b7d4f8649dc9d716cce0c72
jeplasenciap <jeplasenciap@gmail.com>
2017-02-20 13:08:30 +0000

    Corregido error en el nombre de una variable en el gulpfile.

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,10 @@
 var gulp = require('gulp');
-var gh-pages = require('gulp-gh-pages');
+var ghPages = require('gulp-gh-pages');
 
 gulp.task('default', function() {
 
 });
 
 gulp.task('deploy', function() {
-  return gulp.src('./_book/**/*').pipe(gh-pages());
+  return gulp.src('./_book/**/*').pipe(ghPages());
 });
